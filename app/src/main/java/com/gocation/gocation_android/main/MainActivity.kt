8f6ad78e1536896db573719528ce304dcd06f81e
Dylan Lange <dylan@paperkite.co.nz>
2017-05-11 11:12:41 +1200

    Changing to kotlin

---
+++ app/src/main/java/com/gocation/gocation_android/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.gocation.gocation_android.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by dylanlange on 11/05/17.
+ */
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
