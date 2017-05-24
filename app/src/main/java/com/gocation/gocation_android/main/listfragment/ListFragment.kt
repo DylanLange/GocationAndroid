42408c6fe05debb04c5f69a7de4935d1eaeafd1b
Dylan Lange <dylan@paperkite.co.nz>
2017-05-25 10:16:09 +1200

    starting moving to using fragments instead

---
+++ app/src/main/java/com/gocation/gocation_android/main/listfragment/ListFragment.kt
@@ -0,0 +1,16 @@
+package com.gocation.gocation_android.main.listfragment
+
+import android.app.Fragment
+import android.os.Bundle
+
+/**
+ * Created by dylanlange on 25/05/17.
+ */
+
+class ListFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/gocation/gocation_android/main/profilefragment/ProfileFragment.kt
@@ -0,0 +1,16 @@
+package com.gocation.gocation_android.main.profilefragment
+
+import android.app.Fragment
+import android.os.Bundle
+
+/**
+ * Created by dylanlange on 25/05/17.
+ */
+
+class ProfileFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
